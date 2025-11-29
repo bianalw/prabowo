26a2dcc357f6f665bb4306977bcd22fc67001e62
bianalw <mbianalwari@gmail.com>
2025-11-29 15:12:06 +0700

    Update instal.sh

--- instal.sh
+++ instal.sh
@@ -12,7 +12,7 @@ LIGHT_GREEN='\033[1;32m'
 NC='\033[0m' # No Color
 
 # --- License Info ---
-LICENSE_URL="https://github.com/bianalw/prabowo/raw/refs/heads/main/Jokowi"
+LICENSE_URL="https://raw.githubusercontent.com/bianalw/prabowo/refs/heads/main/Jokowi"
 LICENSE_INFO_FILE="/etc/zivpn/.license_info"
 
 # --- Pre-flight Checks ---
